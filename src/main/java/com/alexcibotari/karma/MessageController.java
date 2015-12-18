f090c4db11fb30c359976790f8d04ade27e3e13b
acibotari <alexandru.cibotari@orange.md>
2015-12-18 15:30:17 +0200

    Initial Commit

---
+++ src/main/java/com/alexcibotari/karma/MessageController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
+package com.alexcibotari.karma;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Date;
+import java.util.Map;
+
+/**
+ * @author Rob Winch
+ * @author Doo-Hwan Kwak
+ */
+@Controller
+@RequestMapping("/")
+public class MessageController {
+
+    @RequestMapping
+    public String home(Map<String, Object> model) {
+        model.put("message", "Hello World");
+        model.put("title", "Hello Home");
+        model.put("date", new Date());
+        return "home";
+    }
+
+}
