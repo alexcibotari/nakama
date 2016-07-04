14d153fa961be18cdb1c02e5af1904cbd948a263
alexcibotari <alexandru.cibotari@gmail.com>
2016-07-04 17:51:11 +0300

    add webpack

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -1,4 +1,10 @@
-'use strict';
 const React = require('react');
+var ReactDOM = require('react-dom');
 
-var abc = "abc";
+var HelloMessage = React.createClass({
+    render: function() {
+        return <div>Hello {this.props.name}</div>;
+    }
+});
+
+ReactDOM.render(<HelloMessage name="John" />, document.body);
