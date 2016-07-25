ff2ea9ce51f0ed97cee0c60aa0011e53e4abf921
Andrei JELEZ <ajelez@main.orange.md>
2016-07-25 17:06:34 +0300

    task-13: generated id for the modal

--- src/main/jsx/componenets/project-list.jsx
+++ src/main/jsx/componenets/project-list.jsx
@@ -44,15 +44,14 @@ class ProjectList extends Component {
                             <Link to={'/admin/projects/edit/'+project.id}
                                   className="btn btn-sm btn-default glyphicon glyphicon-pencil"
                                   role="button" title="Edit"/>
-                            <ConfirmationDialog 
+                            <ConfirmationDialog
                                 title="Delete Project."
                                bodyText="Are you sure you want to delete the project?"
                                lunchModalBtnClasses="btn btn-sm btn-danger glyphicon glyphicon-trash"
                                lunchModalBtnText=""
                                lunchModalBtnStyles={{float: 'right'}}
                                actionBtnAction={this.deleteProject.bind(this, project.id)}
-                               modalContainerStyle={{marginLeft: 34+'px'}}
-                               modalId={project.id}/>
+                               modalContainerStyle={{marginLeft: 34+'px'}}/>
                         </div>
                     </td>
                 </tr>
