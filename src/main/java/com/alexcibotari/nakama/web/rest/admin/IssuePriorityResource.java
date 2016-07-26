4401c969db161b9c6ebe939491a42b47edc9cadd
alexcibotari <alexandru.cibotari@gmail.com>
2016-07-26 10:16:01 +0300

    Add Resources related Issue : - Priority - Status - Type

---
+++ src/main/java/com/alexcibotari/nakama/web/rest/admin/IssuePriorityResource.java
@@ -0,0 +1,57 @@
+package com.alexcibotari.nakama.web.rest.admin;
+
+import com.alexcibotari.nakama.service.IssuePriorityService;
+import com.alexcibotari.nakama.service.IssueService;
+import com.alexcibotari.nakama.service.IssueStatusService;
+import com.alexcibotari.nakama.service.IssueTypeService;
+import com.alexcibotari.nakama.web.rest.dto.IssueDTO;
+import com.alexcibotari.nakama.web.rest.dto.IssuePriorityDTO;
+import com.alexcibotari.nakama.web.rest.dto.IssueStatusDTO;
+import com.alexcibotari.nakama.web.rest.dto.IssueTypeDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(path = "/api/admin/issues/priorities", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+public class IssuePriorityResource {
+
+    private final Logger log = LoggerFactory.getLogger(IssuePriorityResource.class);
+
+    @Autowired
+    private IssuePriorityService issuePriorityService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<List<IssuePriorityDTO>> getAll() {
+        return ResponseEntity.ok(issuePriorityService.findAll().stream().map(IssuePriorityDTO::new).collect(Collectors.toList()));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<IssuePriorityDTO> getOne(@PathVariable Long id) {
+        return ResponseEntity.ok(new IssuePriorityDTO(issuePriorityService.findOne(id)));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<IssuePriorityDTO> create(@RequestBody IssuePriorityDTO dto) {
+        return new ResponseEntity<>(new IssuePriorityDTO(issuePriorityService.create(dto)), HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<IssuePriorityDTO> update(@RequestBody IssuePriorityDTO dto) {
+        return ResponseEntity.ok(new IssuePriorityDTO(issuePriorityService.update(dto)));
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        issuePriorityService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+}
