d8c7e09bdbb3af5cf7c9eac52a44ff617bae996f
Andrei JELEZ <ajelez@main.orange.md>
2016-07-27 11:40:11 +0300

    issue type, priority, status management

---
+++ src/main/jsx/componenets/user-sidebar.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+
+
+export default class UserSidebar extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">User options:</div>
+                <div className="panel-body list-group">
+                   
+                </div>
+            </div>
+        )
+    }
+}
+
