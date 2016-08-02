6545d9515b95479c933fa0e29fad39605f558bcd
Valeriu Crudu <valeriu.crudu@orange.md>
2016-08-02 17:35:59 +0300

    DefaultValue changed to value, refactored SelectForm

--- src/main/jsx/componenets/select-form.jsx
+++ src/main/jsx/componenets/select-form.jsx
@@ -2,21 +2,6 @@ import React, {Component} from 'react';
 
 class SelectForm extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            edit: false
-        };
-    }
-
-    editSelect() {
-        this.setState({edit: true});
-    }
-
-    blurSelect() {
-        this.setState({edit: false});
-    }
-
     handleChange(e) {
         if (this.props.onChange) {
             for (var i = 0; i < this.props.options.length; i++) {
@@ -28,15 +13,15 @@ class SelectForm extends Component {
     }
 
     render() {
+        console.log("default:" + this.props.selected.id);
         const options = this.props.options.map(option =>
             <option key={option.id} value={option.id}>{option.name}</option>
         );
         return (
             <select
                 className="form-control"
-                defaultValue={this.props.selected.id}
+                value={this.props.selected.id}
                 onChange={this.handleChange.bind(this)}
-                onBlur={this.blurSelect.bind(this)}
                 title={this.props.selected.description}>
                 {options}
             </select>
