97daefa9a40c77c5d2892d1b2dd0cae477028147
alexcibotari <alexandru.cibotari@gmail.com>
2018-04-09 23:18:49 +0200

    refactoring

--- backend/src/test/java/com/alexcibotari/nakama/ExampleTest.java
+++ backend/src/test/java/com/alexcibotari/nakama/ExampleTest.java
@@ -1,25 +1,14 @@
 package com.alexcibotari.nakama;
 
-import static org.springframework.test.context.TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS;
-
 import com.alexcibotari.nakama.config.ProfileConstants;
-import org.cassandraunit.spring.CassandraDataSet;
-import org.cassandraunit.spring.CassandraUnitDependencyInjectionTestExecutionListener;
-import org.cassandraunit.spring.EmbeddedCassandra;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestExecutionListeners;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@EmbeddedCassandra
-@CassandraDataSet(value = {"cassandra/tables.cql"})
-@TestExecutionListeners(
-  listeners = CassandraUnitDependencyInjectionTestExecutionListener.class,
-  mergeMode = MERGE_WITH_DEFAULTS)
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles(ProfileConstants.UNIT_TEST)
 public class ExampleTest {
